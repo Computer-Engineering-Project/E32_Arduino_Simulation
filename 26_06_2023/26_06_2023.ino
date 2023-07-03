96a951c6e77e6d23f2553355ac3d5947f163df14
boycohol <anhnguyenxil0408@gmail.com>
2023-07-03 16:34:08 +0700

    update receiver

--- 26_06_2023/26_06_2023.ino
+++ 26_06_2023/26_06_2023.ino
@@ -30,6 +30,10 @@ struct Packet {
 
 struct Parameter e32_parameter;
 struct Pin e32_pin;
+String readString;
+String response_data = "test_data";
+bool stringCompleted = false;
+int counter1 = 10;
 
 struct Parameter createParameter(int frequency, int baud_rate, int address, int channel, float air_data_rate, int parity, int transmitting_power) {
   struct Parameter parameter;
@@ -55,7 +59,7 @@ struct Pin createPin(int RXD, int TXD, int M0, int M1, int AUX) {
 }
 
 void inital() {
-  e32_parameter = createParameter(433, 9600, 0x0000, 0x17, 2.4, 8, 100);
+  e32_parameter = createParameter(433, 9600, 0x0010, 0x17, 2.4, 8, 100);
   e32_pin = createPin(0, 1, 2, 3, 4);
 }
 
@@ -102,6 +106,38 @@ struct Packet DecodeStringToPacket(const char* encodeString) {
   return packet;
 }
 
+String sendSetUpInfomation() {
+  String data = "i:";
+  // sprintf(data, "i:%04X:%d", e32_parameter.address, e32_parameter.channel);
+  char hex_param_address[5];
+  sprintf(hex_param_address, "%04X", e32_parameter.address);
+  data = data + String(hex_param_address) + ":" + String(e32_parameter.channel);
+  return data;
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    if (Serial.available() > 0) {
+      char c = (char)Serial.read();  //gets one byte from serial buffer
+      readString += c;
+      if(c=='#'){
+        stringCompleted = true;
+      }               //makes the string readString
+    }
+  }
+  if (readString.length() > 0) {
+    if (readString == "s") {
+      response_data = sendSetUpInfomation();
+    } else {
+      // struct Packet packet = DecodeStringToPacket(readString.c_str());
+      // response_data = EncodePacketToString('b', e32_parameter.address, e32_parameter.channel, packet.owner_address, packet.owner_channel, packet.data);
+      response_data = readString;
+    }
+  } else {
+    response_data = "Can't read! Fail";
+  }
+}
+
 void setup() {
 
   // put your setup code here, to run once:
@@ -119,26 +155,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  String test_data = EncodePacketToString('b', e32_parameter.address, e32_parameter.channel, 25, 17, "test");
-
-  // Send sensor value to Python
-
-  // Serial.println(test_data);
-
-  String readString;
-  while (Serial.available()) {
-    delay(50);
-    if (Serial.available() > 0) {
-      char c = Serial.read();  //gets one byte from serial buffer
-      readString += c;         //makes the string readString
+  counter1--;
+  if (counter1 <= 0) {
+    if (stringCompleted) {
+      counter1 = 1000;
+      Serial.println(response_data);
+      readString = "";
+      response_data = "";
+      counter1 = 1000;
+      stringCompleted = false;
     }
   }
-  if (readString.length() > 0) {
-    struct Packet packet = DecodeStringToPacket(readString.c_str());
-    String response_data = EncodePacketToString('b', e32_parameter.address, e32_parameter.channel, packet.owner_address, packet.owner_channel, packet.data);
-    Serial.println(response_data);
-  }
-  delay(1000);
+  // Serial.println(test_data);
+
+  delay(1);
   // }
 
   // Chờ 1 giây trước khi lặp lại
